9a1ed093a238b3a9d4e819e237596d8dd60e5ae6
Michał Grycki <michalgrycki@MacBook-Pro-Micha.local>
2017-03-26 00:45:22 +0100

    Removing PODS

--- Food Generator/Pods/Target Support Files/Pods-Food GeneratorTests/Pods-Food GeneratorTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Food_GeneratorTests : NSObject
-@end
-@implementation PodsDummy_Pods_Food_GeneratorTests
-@end
--- Food Generator/Pods/Target Support Files/Pods-Food GeneratorUITests/Pods-Food GeneratorUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Food_GeneratorUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Food_GeneratorUITests
-@end
